99d1db5f907587591675e11ad0056e5eca169851
Lawrence <Lawrence.sproul@gmail.com>
2016-05-17 12:29:53 -0600

    change from match to get/post

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Mercury::Engine.routes.draw do
 
   scope '/mercury' do
     get   ':type/:resource',        :to => "mercury#resource"
-    match 'snippets/:name/options', :to => "mercury#snippet_options", :via => [:get, :post]
-    match 'snippets/:name/preview', :to => "mercury#snippet_preview", :via => [:get, :post]
+#    match 'snippets/:name/options', :to => "mercury#snippet_options", :via => [:get, :post], :as => 'snippets_options'
+#    match 'snippets/:name/preview', :to => "mercury#snippet_preview", :via => [:get, :post], :as => 'snippets_preview'
+
+    get 'snippets/:name/options', :to => "mercury#snippet_options"
+    post 'snippets/:name/options', :to => "mercury#snippet_options"
+    get 'snippets/:name/preview', :to => "mercury#snippet_preview"
+    post 'snippets/:name/preview', :to => "mercury#snippet_preview"
+
   end
 end
